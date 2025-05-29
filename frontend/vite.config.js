316c7c7c4a3751635889fbb4a62b2f3772cf93c7
Amila Residovic <amila@example.com>
2025-05-29 18:14:49 +0000

    s

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",         
-    port: 8080,
-    strictPort: true,          
-    watch: {
-      usePolling: true,        
-    },
-    cors: true,                
-    allowedHosts: "all",       
-  },
+  base: "/",
   plugins: [react()],
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: "0.0.0.0",
+    allowedHosts: [
+      "projekat2-alb-2048933755.us-east-1.elb.amazonaws.com",
+      "localhost",
+      "127.0.0.1",
+    ],
+   
+  },
 });
