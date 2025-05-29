c54fbc861cc82f3cd397affcf524fdc244ef7b80
EC2 Default User <ec2-user@ip-172-31-87-49.ec2.internal>
2025-05-29 08:51:15 +0000

    m

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://projekat2-alb-1593865384.us-east-1.elb.amazonaws.com:5000";
+const BACKEND_URL = "http://projekat2-alb-1658197143.us-east-1.elb.amazonaws.com/api";
 
 export async function getContacts() {
   const response = await fetch(`${BACKEND_URL}/contacts`);
