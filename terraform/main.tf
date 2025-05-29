6d3688ff8ef92c43221fec301ea57a412f2ff4a0
EC2 Default User <ec2-user@ip-172-31-87-49.ec2.internal>
2025-05-29 10:01:26 +0000

    m

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "instance_type" {
-  default = "t3.micro"
-}
-
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
